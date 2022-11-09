05b16bd687a5d40839eb065e3e3d1dc7bc3751a3
HarrishElango <58488367+HarrishElango@users.noreply.github.com>
2022-11-09 15:30:46 -0500

    Update Process.java
    adding another constructor class for use in the Priority round robin algorithm because i saw that you made processes with 3 inputs in the final program

--- Process.java
+++ Process.java
@@ -1,10 +1,16 @@
 class Process{//Process class shared between algorithms
     int arrTime;
     int burstTime;
+    int priority;
 
     public Process(int arrTime, int burstTime){
         this.arrTime = arrTime;
         this.burstTime = burstTime;
     }
-
+    
+    public Process(int arrTime, int burstTime, int priority){
+        this.arrTime = arrTime;
+        this.burstTime = burstTime;
+        this.priority = priority;
+    }
 }
